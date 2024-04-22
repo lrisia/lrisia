8cac867edfef19d453dfb4da46cd111b57a6c2e9
lrisia <pochara.su@ku.th>
2024-04-22 14:13:49 +0700

    add more log

--- src/config.js
+++ src/config.js
@@ -6,7 +6,9 @@ class Config {
   }
 
   load() {
+    console.log('Birthday:', process.env.BIRTHDAY);
     const date = process.env.BIRTHDAY.split("-");
+    console.log('Date:', date);
     const birthday = new Date(date[0], date[1] - 1, date[2]);
     return {
       birthday: birthday,
